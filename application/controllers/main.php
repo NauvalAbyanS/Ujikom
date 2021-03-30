84ad842a16e4ad9e7cb08294dc9492566b84e521
NauvalAbyanS <nabyan98.na@gmail.com>
2021-03-30 21:31:01 +0700

    error login

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -3,6 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class main extends CI_Controller {
 	public function index(){
+		$this->form_validation->set_rules('email','Email', 'required|trim|valid_email');
+		$this->form_validation->set_rules('password', 'Password', 'required|trim');
+
 		$this->load->view('auth/login');
 	}
     public function __construct(){
@@ -18,28 +21,34 @@ class main extends CI_Controller {
 		$petugas = $this->db->get_where('petugas',['email'=> $email])->row_array();
 	
 		if($petugas){
-			echo"hallo";
 			if($petugas['id_role'] == 1){
-				if(password_verify($password, 'petugas'['password'])){
-					$data = [
-						'email' => $petugas['email'],
-						'id_role' => $petugas['id_role']
-					];
-					$this->session->set_userdata($data);
-					redirect('user');
+				if(password_verify($password, $petugas['password'])){
+					echo $password;
 				}else{
-					$this->session->set_flashdata('message','<div class="alert alert-danger" 
-					role="alert">Wrong Password!');
-					redirect('auah');
-				}
-			}else{
-				$this->session->set_flashdata('message','<div class="alert alert-danger" 
-				role="alert">this email has not been activated!');
-				redirect('auth');
+						echo "pass salah";
+					}}}}
+					
+					
+					
+					// $data = [
+					// 	'email' => $petugas['email'],
+					// 	'id_role' => $petugas['id_role']
+					// ];
+	// 				$this->session->set_userdata($data);
+	// 				redirect('menu');
+	// 			}else{
+	// 				$this->session->set_flashdata('message','<div class="alert alert-danger" 
+	// 				role="alert">Wrong Password!');
+	// 				redirect('passwordsalah');
+	// 			}
+	// 		}else{
+	// 			$this->session->set_flashdata('message','<div class="alert alert-danger" 
+	// 			role="alert">this email has not been activated!');
+	// 			redirect('auth');
 		
-			}
-		}
-	}
+	// 		}
+	// 	}
+	// }
 	function logout(){
 		$this->session->unset_userdata('email');
 		$this->session->unset_userdata('id_role');
